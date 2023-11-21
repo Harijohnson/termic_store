e0d5ddbe271d64806beb037f7f2dca8ba129afb1
Harijohnson <harijn72@gmail.com>
2023-11-21 19:03:04 +0530

    base structuser is created ;

--- termic_shop/src/App.js
+++ termic_shop/src/App.js
@@ -1,15 +1,14 @@
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import  HomeScreen from './screens/HomeScreen'
 function App() {
   return (
     <div >
       <Header />
         <main className = 'py-5'>
           <Container>
-            <h1>
-            Welcome to Termic Shop
-            </h1>
+            <HomeScreen />
           </Container>
         </main>
       <Footer />
