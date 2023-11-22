8308eda37baf7a13914f3657d420d7bd98ace39b
Harijohnson <harijn72@gmail.com>
2023-11-22 09:27:55 +0530

    rating component is working

--- termic_shop/src/components/Header.js
+++ termic_shop/src/components/Header.js
@@ -15,8 +15,8 @@ function Header() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i></Nav.Link>
-                    <Nav.Link href="/login"><i className='fas fa-user'></i></Nav.Link>
+                    <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                    <Nav.Link href="/login"><i className='fas fa-user'></i> Login</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -27,3 +27,27 @@ function Header() {
 }
 
 export default Header
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
