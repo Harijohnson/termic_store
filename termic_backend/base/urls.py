8719c70a4de7b7c6b553e2c3599b160617d0c179
Harijohnson <harijn72@gmail.com>
2023-11-23 22:36:30 +0530

    encoding the user data is done bu jwt serializer with hs256 hashing method

--- termic_backend/base/urls.py
+++ termic_backend/base/urls.py
@@ -18,7 +18,9 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name='routes'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product'),
