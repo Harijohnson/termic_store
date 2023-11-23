e6b90d44e59ba9f3bbdddd1765d0e6cfb1348aa7
Harijohnson <harijn72@gmail.com>
2023-11-23 08:13:21 +0530

    error loading page iis done

--- termic_shop/src/actions/productActions.js
+++ termic_shop/src/actions/productActions.js
@@ -6,7 +6,7 @@ import {
 
 import axios from 'axios'
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try{
         dispatch({type:PRODUCT_LIST_REQUEST})
         const { data } = await axios.get('/api/products/')
