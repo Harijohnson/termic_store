7c351b913b851671254e0a9382e29a92c4ba933f
Harijohnson <harijn72@gmail.com>
2023-11-25 16:07:04 +0530

    update profile is done

--- termic_shop/src/actions/userActions.jsx
+++ termic_shop/src/actions/userActions.jsx
@@ -14,6 +14,11 @@ import {
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAILE,
 
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAILE,
+    USER_UPDATE_PROFILE_RESET,
+
         } from '../constants/userConstants'
 import axios  from 'axios'
 
@@ -141,6 +146,57 @@ export  const  getUserDetails = ( id ) => async (dispatch,getState) => {
     catch(error){
         dispatch({
             type:    USER_DETAILS_FAILE,
+            payload:error.response && error.response.data.detail
+            ?
+            error.response.data.detail:
+            error.detail,
+        })
+    }
+}
+
+
+
+export  const  updateUserProfile = ( user ) => async (dispatch,getState) => {
+    try{
+        dispatch({
+            type:USER_UPDATE_PROFILE_REQUEST,
+    })
+
+    const {
+        userLogin : { userInfo },
+    }=getState()
+
+
+
+    const config = {
+        headers:{
+            'Content-type':'application/json',
+            Authorization  :`Bearer ${userInfo.token}`
+        }
+
+    }
+    const {data} = await axios.put(
+        `/api/users/profile/update/`,
+        user,
+        config
+        )
+    
+    dispatch({
+        type:USER_UPDATE_PROFILE_SUCCESS,
+        payload:data,
+    })
+    
+    dispatch({
+        type:USER_LOGIN_SUCCESS,
+        payload:data,
+    })
+
+    localStorage.setItem('userInfo',JSON.stringify(data))
+    
+    }
+    catch(error){
+        dispatch({
+            type:    USER_UPDATE_PROFILE_FAILE,
             payload:error.response && error.response.data.detail
             ?
             error.response.data.detail:
