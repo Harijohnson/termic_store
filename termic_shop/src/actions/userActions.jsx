a8321c2583b65da9208a14083b91b4fefc889019
Harijohnson <harijn72@gmail.com>
2023-12-03 14:37:55 +0530

    admin update user and delete user is working for now

--- termic_shop/src/actions/userActions.jsx
+++ termic_shop/src/actions/userActions.jsx
@@ -26,7 +26,13 @@ import {
 
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
-    USER_DELETE_FAILE,} from '../constants/userConstants'
+    USER_DELETE_FAILE,
+
+
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAILE,
+    USER_UPDATE_RESET,} from '../constants/userConstants'
 import axios  from 'axios'
 
 
@@ -306,6 +312,57 @@ export  const  deleteUser = (id) => async (dispatch,getState) => {
     catch(error){
         dispatch({
             type:    USER_DELETE_FAILE,
+            payload:error.response && error.response.data.detail
+            ?
+            error.response.data.detail:
+            error.detail,
+        })
+    }
+}
+
+
+
+
+
+export  const  updateUser = (user) => async (dispatch,getState) => {
+    try{
+        dispatch({
+            type:USER_UPDATE_REQUEST,
+    })
+
+    const {
+        userLogin : { userInfo },
+    }=getState()
+
+
+
+    const config = {
+        headers:{
+            'Content-type':'application/json',
+            Authorization  :`Bearer ${userInfo.token}`
+        }
+
+    }
+    const {data} = await axios.put(
+        `/api/users/update/${user._id}/`,
+        user,
+        config
+        )
+    
+    dispatch({
+        type:USER_UPDATE_SUCCESS,
+    })
+    
+
+    dispatch({
+        type:USER_DETAILS_SUCCESS,
+        payload: data,
+    })
+    
+    }
+    catch(error){
+        dispatch({
+            type:    USER_UPDATE_FAILE,
             payload:error.response && error.response.data.detail
             ?
             error.response.data.detail:
--- termic_shop/src/store.js
+++ termic_shop/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import {productListReducer,productDetailsReducer} from './reducers/productReducers'
 import { cartReducer, } from './reducers/cartReducers'
 import { userLoginReducer } from './reducers/userReducers'
-import { userRegisterReducer,userDetailsReducer,userUpdateProfileReducer,userListReducer,userDeleteReducer } from './reducers/userReducers'
+import { userRegisterReducer,userDetailsReducer,userUpdateProfileReducer,userListReducer,userDeleteReducer,userUpdateReducer } from './reducers/userReducers'
 import { orderCreateReducer,orderDetailsReducer,orderPayReducer,orderListMyReducer } from './reducers/orderReducer'
 
 const reducer = combineReducers({
@@ -21,6 +21,7 @@ const reducer = combineReducers({
     orderListMy:orderListMyReducer,
     userList:userListReducer,
     userDelete:userDeleteReducer,
+    userUpdate:userUpdateReducer,
 
 })
 
