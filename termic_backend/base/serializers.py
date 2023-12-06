c54308388a9465bbff71f3293848eb5bb9c67a50
Harijohnson <harijn72@gmail.com>
2023-12-06 13:03:10 +0530

    add review backend is dine

--- termic_backend/base/serializers.py
+++ termic_backend/base/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Product,Order,OrderItem,ShippingAddress,Product
+from .models import Product,Order,OrderItem,ShippingAddress,Product,Review
 from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserSerializer(serializers.ModelSerializer):
@@ -35,11 +35,41 @@ class UserSerializerWithToken(UserSerializer):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
 
+
+
+
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields =  '__all__'
+
+
+
+
+
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields =  '__all__'
+    
 
+    def get_reviews(self,obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews,many=True)
+        return serializer.data
+    
+
+
+
+
+
+    
 class ShippingAddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShippingAddress
